21e756c2a4eb7b2ced747a8adde36f736d78a00a
yuchokr <yujin.cho.kr@gmail.com>
2026-02-24 19:29:44 +0100

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,40 @@
+import sparrow as sp
+from spatialdata import read_zarr
+
+zarr_path = "larger_subset.zarr"
+sdata = read_zarr(zarr_path)
+
+img_layer = list(sdata.images.keys())[0]
+print("Input image layer:", img_layer)
+
+z_vis = 3.5
+
+# min-max filtering
+sdata = sp.im.min_max_filtering(
+    sdata=sdata,
+    img_layer=img_layer,
+    output_layer="min_max_filtered",
+    size_min_max_filter=80,
+    overwrite=True,
+)
+
+# CLAHE
+sdata = sp.im.enhance_contrast(
+    sdata=sdata,
+    img_layer="min_max_filtered",
+    output_layer="clahe",
+    contrast_clip=15.0,
+    chunks=20000,
+    overwrite=True,
+)
+
+# quick check plot (3 layers in one figure)
+sp.pl.plot_shapes(
+    sdata,
+    img_layer=[img_layer, "min_max_filtered", "clahe"],
+    shapes_layer=None,
+    z_slice=z_vis,
+    channel="DAPI",
+    figsize=(18, 6),
+    img_title=True,
+)
